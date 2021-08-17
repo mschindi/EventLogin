6ab8f70ccc04b8befa19cee2255b97fbb5fdfb0e
Michael Schindezow <michaelschindezow@MacBook-Pro-von-Michael.local>
2021-08-17 11:36:00 +0200

    fiveth commit

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,14 +1,54 @@
+using System;
+using System.Threading.Tasks;
 using EventLogin.Entities;
 using EventLogin.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EventLogin.Controllers
 {
-    public class UserController : ControllerBase
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
     {
+        private readonly IEntityService<UserEntity> service;
+
         public UserController(IEntityService<UserEntity> service )
         {
-            
+            this.service = service;
         }
+        
+        
+        // CREATE
+        [HttpPost]
+        public async Task<ActionResult> Add([FromBody] UserEntity userEntity)
+        {
+            var entity = await this.service.Add(userEntity);
+            return Ok(entity);
+        }
+        
+        // READ
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var entities = await this.service.GetAll();
+            return this.Json(entities);
+        }
+        
+        // UPDATE
+        [HttpPut]
+        public  ActionResult Update([FromBody] UserEntity userEntity)
+        {
+            this.service.Update(userEntity);
+            return this.Ok();
+        }
+        
+        // GET BY ID
+        [HttpGet("{id:Guid}")]
+        public async Task<ActionResult> GetById(Guid id)
+        {
+            var entity = await this.service.GetById(id);
+            return Ok(entity);
+        }
+
     }
 }
